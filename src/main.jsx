3211216402feeb9934f52274bbd3914bfdd199b1
Dedios <dediosprogramming@gmail.com>
2024-06-27 14:54:25 -0400

    successfully implemented the service-worker

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+// service worker registration
+if ('serviceWorker' in navigator) {
+  const scope = location.pathname.replace(/\/[^/]+$/, '/');
+  navigator.serviceWorker
+    .register('service-worker.js', { scope, type: 'module' })
+    .then(function (reg) {
+      reg.addEventListener('updatefound', function () {
+        const installingWorker = reg.installing;
+        console.log(
+          'A new service worker is being installed:',
+          installingWorker
+        );
+      });
+      console.log('Registration succeeded. Scope is ' + reg.scope);
+    })
+    .catch(function (error) {
+      console.log('Registration failed with ' + error);
+    });
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,1 +1,16 @@
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
 
+  if (url.pathname === '/api/hello') {
+    const headers = {
+      'Content-Type': 'text/plain',
+    };
+    const greeting = "Hey it's ya service workers";
+
+    event.respondWith(textResponse(greeting, headers));
+  }
+});
+
+const textResponse = (message, headers) => {
+  return new Response(message, headers);
+};
