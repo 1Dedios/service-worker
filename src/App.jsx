85e0708d9c654dd96a2586fc7492384296d0b339
Dedios <dediosprogramming@gmail.com>
2024-08-09 17:08:20 -0400

    implementing auth on endpoint '/api/login'

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,70 @@
+import { useState, useRef } from 'react';
 import './App.css';
 
+// TODO: implement '/api/login' endpoint for auth
+const login = (username, password) => {
+  return fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ username, password }),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(JSON.parse(data)));
+};
+
 function App() {
+  // TODO: implement state of the form, capture form values with useRef, handlesubmit function
+  const [token, setToken] = useState(null);
+  const [error, setError] = useState(null);
+  const usernameRef = useRef('');
+  const passwordRef = useRef('');
+
+  const auth = async (e) => {
+    e.preventDefault();
+
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+    const res = await login(username, password);
+
+    if (res.result) {
+      setToken(res.result);
+    } else if (res.error) {
+      setError(res.error);
+    }
+
+    usernameRef.current.value = '';
+    passwordRef.current.value = '';
+  };
+
   return (
     <>
-      <form>
-        <div id="username">
-          <label htmlFor="username">Username: </label>
-          <input id="username" placeholder="Username"></input>
-        </div>
-        <div id="password">
-          <label htmlFor="password">Password: </label>
-          <input id="password" placeholder="password"></input>
-        </div>
-        <button id="login-button">Login</button>
-      </form>
+      <div>
+        {!token && (
+          <form onSubmit={auth}>
+            <div id="username">
+              <label htmlFor="username">Username: </label>
+              <input
+                id="username"
+                ref={usernameRef}
+                placeholder="Username"
+              ></input>
+            </div>
+            <div id="password">
+              <label htmlFor="password">Password: </label>
+              <input
+                id="password"
+                ref={passwordRef}
+                placeholder="password"
+              ></input>
+            </div>
+            <button id="login-button">Login</button>
+          </form>
+        )}
+        {error && <p className="error">{error}</p>}
+      </div>
     </>
   );
 }
--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,5 @@
+import jwt from './jwt.js';
+
 const textResponse = (message, headers) => {
   return new Response(message, headers);
 };
@@ -16,11 +18,25 @@ self.addEventListener('fetch', (event) => {
     event.respondWith(textResponse(greeting, headers));
   } else if (url.pathname === '/api/login') {
     console.log('Request is: ', url.pathname);
-    const header = {
-      'Content-Type': 'text/plain',
-    };
-    const loginMessage = 'Welcome to the login service';
 
-    event.respondWith(textResponse(loginMessage, header));
+    //TODO: Implement verification of the token
+    /**
+     * COMMENT ABOUT VERIFICATION OF TOKEN:
+     *
+     * Of course, in a real application, we would query our db with something like - 'SELECT username, password FROM users WHERE username=username AND password=password'
+     *
+     * If the response from the db is successful (retrieved a record with the above matching) then we would sign the token and return.
+     * Otherwise, we would deny authorization.
+     *
+     */
+    async (req, res) => {
+      const { username, password } = req.json() ?? {};
+      if (username === 'demo' && password === 'demo') {
+        const token = await jwt.sign({ username });
+        return res.json({ result: token });
+      } else {
+        res.json({ error: 'Invalid username or password' });
+      }
+    };
   }
 });
